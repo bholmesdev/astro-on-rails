ef908cd6389bb14a8d79772d75a1fbc00bcc7d9b
bholmesdev <hey@bholmes.dev>
2024-06-02 22:03:00 -0400

    feat: render blog articles with astro

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,19 +3,25 @@ require 'uri'
 
 class ArticlesController < ApplicationController
   def index
-    props = { :message => "Welcome to Rails, Astro" }
-    view = "message"
+    articles = Article.all
+    props = { :articles => articles }
+    view = "articles/index"
     uri = URI.parse("http://localhost:4321?props=#{props.to_json}&view=#{view}")
     res = Net::HTTP.get_response uri
+
+    puts res.body
 
     render html: res.body.html_safe
   end
 
   def show
-    # context = MiniRacer::Context.new
-    # context.load("/Users/benholmes/Sandbox/rails-blog/app/controllers/test.mjs")
-    # puts context.call("adder", 1, 2)
+    article = Article.find(params[:id])
+    props = article
+    view = "articles/show"
 
-    @article = Article.find(params[:id])
+    uri = URI.parse("http://localhost:4321?props=#{props.to_json}&view=#{view}")
+    res = Net::HTTP.get_response uri
+
+    render html: res.body.html_safe
   end
 end
