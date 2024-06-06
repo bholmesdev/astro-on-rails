764fee0758f3e7e1d68389a46ad67d8f027149a4
bholmesdev <hey@bholmes.dev>
2024-06-06 16:36:24 -0400

    refactor: move view to special header

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,9 @@ require 'uri'
 class ArticlesController < ApplicationController
   def index
     articles = Article.all
-    props = { :articles => articles, :view => "articles/index" }
+    props = { :articles => articles }
 
+    response.headers['X-Astro-View'] = "articles/index"
     render json: props
   end
 
@@ -13,10 +14,10 @@ class ArticlesController < ApplicationController
     article = Article.find(params[:id])
     props = {
       :title => article.title,
-      :body => article.body,
-      :view => "articles/show"
+      :body => article.body
     }
 
+    response.headers['X-Astro-View'] = "articles/show"
     render json: props
   end
 end
