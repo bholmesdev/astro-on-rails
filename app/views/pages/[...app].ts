df3b51c38a675f278bcfd63c81fa70853f8e92e5
bholmesdev <hey@bholmes.dev>
2024-06-06 16:37:50 -0400

    refactor: .astro -> catch all route

---
+++ app/views/pages/[...app].ts
@@ -0,0 +1,28 @@
+import type { APIRoute } from "astro";
+
+export const ALL: APIRoute = async (ctx) => {
+  const rubyResponse = await fetch(
+    new URL(ctx.url.pathname, "http://localhost:3000")
+  );
+
+  if (!rubyResponse.ok) {
+    return rubyResponse;
+  }
+  const view = rubyResponse.headers.get("X-Astro-View");
+  if (!view) {
+    return rubyResponse;
+  }
+  let props = {};
+  const contentType = rubyResponse.headers.get("Content-Type");
+  const baseType = contentType?.split(";")?.[0]?.toLowerCase();
+  if (baseType === "application/json") {
+    props = await rubyResponse.json();
+  }
+
+  const rewriteUrl = new URL(
+    "/views/" + view.replace("index", ""),
+    ctx.url.origin
+  );
+  ctx.locals.rubyProps = props;
+  return ctx.rewrite(rewriteUrl);
+};
